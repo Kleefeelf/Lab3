067108607fd9f60e629b1f8ba127131fc976d238
Kleefeelf <74529601+Kleefeelf@users.noreply.github.com>
2021-04-27 14:22:13 +0300

    Updated CounterBarManager

--- manager/CounterBarManager.java
+++ manager/CounterBarManager.java
@@ -1,13 +1,15 @@
 package ua.lviv.iot.bar.manager;
 
-import ua.lviv.iot.bar.models.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ua.lviv.iot.bar.models.Item;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PUBLIC)
@@ -17,22 +19,21 @@ import lombok.experimental.FieldDefaults;
 public final class CounterBarManager {
     List<Item> items;
 
-    public List<Item> searchByGlassMaterial(final String material){
+    public List<Item> searchByGlassMaterial(final String material) {
         return items.stream().filter(item ->
                 item.getMaterial().equals(material)).collect(Collectors.toList());
     }
 
-    public void sortByPrice(boolean reversed){
+    public void sortByPrice(boolean reversed) {
         if (reversed) {
             items.sort(Comparator.comparing(Item::getPrice));
         }
         else {
             items.sort(Comparator.comparing(Item::getPrice).reversed());
         }
-
     }
 
-    public void sortByWeight(boolean reversed){
+    public void sortByWeight(boolean reversed) {
         if (reversed) {
             items.sort(Comparator.comparing(Item::getWeight));
         }
